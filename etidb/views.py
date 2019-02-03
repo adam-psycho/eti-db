e7d2db10ef19d08aaec021b6c1cebbbf5fd9e7ab
adam-psycho <adamvoliva+1@gmail.com>
2019-02-02 20:55:05 -0800

    Add tasker.

--- etidb/views.py
+++ etidb/views.py
@@ -3,10 +3,27 @@ import datetime
 import albatross
 from django.http import HttpResponse
 from etidb import models;
+from background_task import background
 
 etiConn = albatross.Connection(username="podrick", password="luelinks")
 
 def query_users(request):
+    bg_users()
+
+    return HttpResponse();
+
+
+def query_topics(request):
+    # November 1st 2018, set to Eastern timezone
+
+    bg_topics(request.GET.get('start_time'), request.GET.get('end_time'))
+
+    return HttpResponse("Running")
+
+
+
+@background(schedule=1)
+def bg_users():
     literally_all_users = etiConn.users().search(recurse=True)
     eti_users = [];
     for user in literally_all_users:
@@ -16,17 +33,13 @@ def query_users(request):
 
     models.EtiUser.objects.bulk_create(eti_users)
 
-    return HttpResponse();
 
-
-def query_topics(request):
-    # November 1st 2018, set to Eastern timezone
+@background(schedule=1)
+def bg_topics(start_time, end_time):
     tz = pytz.timezone('US/Pacific')
-    activeSince = datetime.datetime.strptime(
-        request.GET.get('start_time'), '%Y-%m-%d').replace(tzinfo=tz)
-    maxTime = datetime.datetime.strptime(
-        request.GET.get('end_time'), '%Y-%m-%d').replace(tzinfo=tz)
 
+    activeSince = datetime.datetime.strptime(start_time, '%Y-%m-%d').replace(tzinfo=tz)
+    maxTime = datetime.datetime.strptime(end_time, '%Y-%m-%d').replace(tzinfo=tz)
     topics = etiConn.topics(allowedTags=["LUE"], forbiddenTags=["Anonymous"]).search(
         activeSince=activeSince, maxTime=maxTime, recurse=True)
 
@@ -35,6 +48,7 @@ def query_topics(request):
     updated = 0
     for topic in topics:
         t = models.EtiTopic.objects.filter(id=topic.id)
+        print('{}: {} @ {}'.format(topic.id, topic.title, topic.lastPostTime.strftime('%Y-%m-%d')))
         if (len(t)):
             updated += 1
             t.update(
@@ -53,7 +67,3 @@ def query_topics(request):
                 post_count=topic.postCount,
                 eti_user=models.EtiUser(topic.user.id),
                 created=topic.lastPostTime)
-
-
-
-    return HttpResponse("Created: {}\nUpdate: {}".format(created, updated))
